423a1689961437566b743e633ecce07f9db36fa5
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:18 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,44 @@ output "orbital_contact_profiles" {
   description = "All orbital_contact_profile resources"
   value       = azurerm_orbital_contact_profile.orbital_contact_profiles
 }
+output "orbital_contact_profiles_auto_tracking" {
+  description = "List of auto_tracking values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.auto_tracking]
+}
+output "orbital_contact_profiles_event_hub_uri" {
+  description = "List of event_hub_uri values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.event_hub_uri]
+}
+output "orbital_contact_profiles_links" {
+  description = "List of links values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.links]
+}
+output "orbital_contact_profiles_location" {
+  description = "List of location values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.location]
+}
+output "orbital_contact_profiles_minimum_elevation_degrees" {
+  description = "List of minimum_elevation_degrees values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.minimum_elevation_degrees]
+}
+output "orbital_contact_profiles_minimum_variable_contact_duration" {
+  description = "List of minimum_variable_contact_duration values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.minimum_variable_contact_duration]
+}
+output "orbital_contact_profiles_name" {
+  description = "List of name values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.name]
+}
+output "orbital_contact_profiles_network_configuration_subnet_id" {
+  description = "List of network_configuration_subnet_id values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.network_configuration_subnet_id]
+}
+output "orbital_contact_profiles_resource_group_name" {
+  description = "List of resource_group_name values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.resource_group_name]
+}
+output "orbital_contact_profiles_tags" {
+  description = "List of tags values across all orbital_contact_profiles"
+  value       = [for k, v in azurerm_orbital_contact_profile.orbital_contact_profiles : v.tags]
+}
 
